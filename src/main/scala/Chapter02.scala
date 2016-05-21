6962acc16cdfb0303bc9da007af938d310ce5b36
sh0hei <vividbit@gmail.com>
2016-05-21 22:53:44 +0900

    Scala REPL introduction Chapter02

--- src/main/scala/Chapter02.scala
+++ src/main/scala/Chapter02.scala
@@ -3,4 +3,14 @@
   */
 object Chapter02 {
 
+  def run(): Unit = {
+
+    // Scala REPL
+    // scala> 1 + 2
+    // res0: Int = 3 // Scala has package same as Java
+    // res0 * 3      // Scala has Class corresponding Java primitive types
+    // res1: 9       // scalac use Java primitive types as much as possible when compile Scala code to Java bytecode
+    // println("Hello, world")
+    // Hello, world!
+  }
 }
